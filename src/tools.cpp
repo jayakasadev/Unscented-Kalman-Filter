6e4f24a66fb0e7da5f1c57b9ebda4338d1b15c62
Jaya Kasa <kasa288@gmail.com>
2017-08-30 16:19:59 -0400

    completed RMSE function

--- src/tools.cpp
+++ src/tools.cpp
@@ -9,10 +9,37 @@ Tools::Tools() {}
 
 Tools::~Tools() {}
 
-VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+/**
+ * Method for calculating the RMSE for given inputs
+ *
+ * @param estimations
+ * @param ground_truth
+ * @return
+ */
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth) {
+    // output vector
+    VectorXd rmse(4);
+    rmse.fill(0);
+
+    // if there are no estimations or the number of estimations do not match the number of
+    // ground truths, return empty rmse
+    if(estimations.size() == 0 || estimations.size() != ground_truth.size()){
+        cout << "\n\tError: Invalid estimation or ground_truth data" << endl;
+        return rmse;
+    }
+
+
+    // accumulate squared residuals
+    for(unsigned int  i = 0; i < estimations.size(); i++){
+        // getting the difference between the calculated and expected
+        VectorXd residual = estimations[i] - ground_truth[i];
+
+        residual = residual.array().square();
+        rmse += residual;
+    }
+
+    // averaging over the number of estimates and returning the square root
+    rmse = (rmse / estimations.size()).array().sqrt();
+
+    return rmse;
 }
